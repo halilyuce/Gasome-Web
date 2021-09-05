86865d7f8721320b9192b92681a3ad34ed49e2a0
Ömer Yüce <oomeryuce@gmail.com>
2021-09-06 00:08:28 +0300

    Games page added

---
+++ components/Games/GameCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="w-full md:mb-36">
+    <div class="col-span-12 relative">
+      <div
+        class="w-full h-72 rounded-xl static"
+        :class="game.screenshots.length > 0 ? '' : 'bg-purple-700'"
+        :style="cover"
+        style="filter: blur(5px);"
+      ></div>
+      <div class="absolute top-32 left-10 md:left-32 flex">
+        <img :src="`${mediumImagePath + game.image}.webp`" alt="Game Image" class="h-36 md:h-72 ring ring-2 ring-white dark:ring-black rounded-lg shadow-lg">
+        <div class="flex flex-col ml-5 text-white">
+          <h1 class="text-xl md:text-5xl my-1 stroke-black">{{ game.name }}</h1>
+          <span class="my-1 stroke-black text-sm md:text-lg lg:3xl">{{ $moment(game.release_date).format('ll') }} ({{ $moment(game.release_date).fromNow() }})</span>
+          <div v-if="game.company" class="flex items-center my-1 stroke-black">
+            <vs-avatar circle class="mr-2" size="40">
+              <img :src="`${companyImagePath + game.company.logo}.webp`" alt="" class="h-6 w-6">
+            </vs-avatar>
+            <span class="stroke-black text-sm md:text-lg lg:text-2xl">
+              {{ game.company ? game.company.name : '' }}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "GameCard",
+  props: {
+    game: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    cover(){
+      if (this.game.screenshots.length > 0){
+        return `background: url(${this.gameSSPath + this.game.screenshots[0]+ '.jpg'}) no-repeat center; background-size: 100% auto;`;
+      }
+      return ''
+    }
+  },
+  data() {
+    return {
+      smallImagePath: process.env.GAMEIMAGE_SMALL,
+      mediumImagePath: process.env.GAMEIMAGE_MEDIUM,
+      companyImagePath: process.env.COMPANYIMAGE_SMALL,
+      gameSSPath: process.env.GAMESS_MEDIUM,
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .stroke-black {
+    text-shadow:
+      -1px -1px 1px #000,
+      1px -1px 1px #000,
+      -1px 1px 1px #000,
+      1px 1px 1px #000;
+  }
+</style>
---
+++ components/Rating/StarRate.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div v-if="point > 0" class="grid grid-cols-12">
+      <span class="font-bold col-span-3 flex items-center" :class="size === 'sm' ? 'text-md' : 'text-2xl'">{{ point }}/5</span>
+      <vs-button-group class="col-span-9 items-center" dark>
+        <vs-button v-for="i in 5" transparent :active-disabled="disabled" :size="size === 'sm' ? 'small' : 'default'">
+          <i
+            class='bx'
+            :class="
+            [point > i || point === 5
+            ? 'bxs-star'
+            : (
+              point > (i - 0.5) && point < (i + 1)
+              ? 'bxs-star-half'
+              : 'bx-star'
+              ), size === 'sm' ? 'text-md' : 'text-3xl']
+            "
+            :id="i"
+          ></i>
+        </vs-button>
+      </vs-button-group>
+    </div>
+    <div v-else>
+      <p>Nobody rated this game yet.</p>
+      <vs-button class="m-0">Be first!</vs-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StarRate",
+  data() {
+    return {
+      hover: null,
+    }
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    point: {
+      type: Number
+    },
+    size: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped></style>
