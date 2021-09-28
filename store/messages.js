31ad68e2ef89511275dd203412292c1cd334233e
Halil YÜCE <hllibrhmyc@gmail.com>
2021-09-29 00:58:54 +0100

    UI Improvements

--- store/messages.js
+++ store/messages.js
@@ -1,6 +1,8 @@
 export const state = () => ({
   loading: false,
+  messagesLoading: false,
   contacts: [],
+  messages: [],
   selected: null,
 })
 export const getters = {}
@@ -11,12 +13,27 @@ export const mutations = {
   setContacts(state, payload) {
     state.contacts = [...state.contacts, ...payload]
   },
+  setMessages(state, payload) {
+    state.messages = [...state.messages, ...payload].reverse()
+  },
+  setMessagesVal(state, payload) {
+    state.messages = payload
+  },
   setSelected(state, payload) {
     state.selected = payload
+    const index = state.contacts.findIndex((item) => item === payload)
+    if (index > -1) {
+      var element = state.contacts[index]
+      state.contacts.splice(index, 1)
+      state.contacts.splice(0, 0, element)
+    }
+  },
+  setMessagesLoading(state, payload) {
+    state.messagesLoading = payload
   },
 }
 export const actions = {
-  async getContacts({ dispatch, commit }, payload) {
+  async getContacts({ dispatch, commit }) {
     await commit('setLoading', true)
     try {
       const response = await this.$axios.get('/api/messagebox')
@@ -31,7 +48,27 @@ export const actions = {
       throw 'Unable to load contacts'
     }
   },
+  async getMessages({ dispatch, commit }, payload) {
+    await commit('setMessagesLoading', true)
+    try {
+      const response = await this.$axios.get(
+        '/api/conversation/' + payload.id + '?page=' + payload.page
+      )
+      commit('setMessages', response.data.data.data)
+      commit('setMessagesLoading', false)
+      return response.data.data
+    } catch (error) {
+      dispatch('alert/error', error.response, {
+        root: true,
+      })
+      commit('setMessagesLoading', false)
+      throw 'Unable to load messages'
+    }
+  },
   async setSelected({ commit }, payload) {
     commit('setSelected', payload)
+  },
+  async setMessages({ commit }, payload) {
+    commit('setMessagesVal', payload)
   },
 }
