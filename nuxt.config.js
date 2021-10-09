dffd7a4142f5d53337b36b73b2955a248b13fbb9
Halil İbrahim YÜCE <hllibrhmyc@gmail.com>
2021-10-10 00:28:02 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 export default {
+  target: "https://gasome.com"
   ssr: false,
   router: {
     base: '/',
@@ -7,7 +8,7 @@ export default {
   },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Gasome',
+    title: 'Gasome - Gamer Social Media',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
